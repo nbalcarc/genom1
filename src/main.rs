bcca2a06f61793a08ef287dfdc4ba63fade25700
nbalcarc <nathan.balcarcel@wsu.edu>
2022-12-11 03:47:07 -0800

    Began experimenting with Rust directory reading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use crate::structs::PhyloTree;
 
+use std::{path::Path, env, fs};
+
 mod algorithms;
 mod errors;
 mod structs;
@@ -11,6 +13,16 @@ fn main() {
     println!("{}", algorithms::levenshtein("alderkitten", "kitten"));
     dbg!(algorithms::file_size("/home/terrior/Programming/genome-tree/src/test.txt"));
 
-    algorithms::generate_kmers(file_dir, k, num)
+    //algorithms::generate_kmers(file_dir, k, num);
+    let x = env::current_dir().unwrap().to_str().unwrap().to_owned();
+    let dir = env::current_dir().unwrap().as_path().display().to_string();
+    println!("{}", x);
+    println!("{}", dir);
+    let paths = fs::read_dir(x).unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+
+
     let tree = PhyloTree::new();
 }
